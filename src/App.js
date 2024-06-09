73a75f68523449120854734414955d584e9f3e1a
rishabhtdubey <mailcoder536@gmail.com>
2024-06-09 13:18:24 +0530

    created date counter mini project to understand states in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Counter = () => {
+
+    const [step, setStep] = useState(1);
+    const [count, setCount] = useState(0);
+    const date = new Date();
+    date.setDate(date.getDate() + count);
+    
+    
+    // let date = new Date().getDate();
+    // let month = new Date().getMonth() + 1;
+    // let year = new Date().getFullYear();
+
+
+    return (
+      <div className='counter'>
+        <div>
+            <button onClick={() => setStep((s) => s-1)}>-</button>
+            <span>Step: {step}</span>
+            <button onClick={() => setStep((s) => s+1)}>+</button>
+        </div>
+
+        <div>
+            <button onClick={() => setCount((c) => c-step)}>-</button>
+            <span>Count: {count}</span>
+            <button onClick={() => setCount((c) => c+step)}>+</button>
+        </div>
+
+        <p>{date.toDateString()}</p>
+
+      </div>  
+    )
 }
 
-export default App;
+const App = () => {
+  return (
+    <Counter/>
+  )
+}
+
+export default App
